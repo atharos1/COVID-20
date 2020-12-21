d719c8c39b7609d430f1e458d3b47a5ae99cb533
atharos1 <nbarrera@itba.edu.ar>
2020-12-21 12:10:51 -0300

    Arreglo

--- COVID-20/Controllers/ConfigurationController.cs
+++ COVID-20/Controllers/ConfigurationController.cs
@@ -71,124 +71,127 @@ namespace COVID_20.Controllers
 
             var pageToGet = "https://sisa.msal.gov.ar/datos/descargas/covid-19/files/Covid19Casos.csv";
 
-            using (var client = new HttpClient())
-            using (HttpResponseMessage response = await client.GetAsync(pageToGet))
-            using (HttpContent content = response.Content)
-            using (var stream = (MemoryStream)await content.ReadAsStreamAsync())
-            using (var reader = new StreamReader(stream))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture)) {
-                csv.Read();
-                csv.ReadHeader();
+            using (var client = new HttpClient()) {
+                client.Timeout = TimeSpan.FromSeconds(200);
 
-                while (csv.Read()) {
+                using (HttpResponseMessage response = await client.GetAsync(pageToGet))
+                using (HttpContent content = response.Content)
+                using (var stream = (MemoryStream)await content.ReadAsStreamAsync())
+                using (var reader = new StreamReader(stream))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture)) {
+                    csv.Read();
+                    csv.ReadHeader();
 
-                    if (csv.GetField<DateTime>("ultima_actualizacion") < lastUpdated)
-                        continue;
+                    while (csv.Read()) {
 
-                    if (!provinceMap.ContainsKey(csv.GetField<int>("residencia_provincia_id"))) {
-                        var n = new Province {
-                            CsvID = csv.GetField<int>("residencia_provincia_id"),
-                            Name = csv.GetField<string>("residencia_provincia_nombre")
+                        if (csv.GetField<DateTime>("ultima_actualizacion") < lastUpdated)
+                            continue;
+
+                        if (!provinceMap.ContainsKey(csv.GetField<int>("residencia_provincia_id"))) {
+                            var n = new Province {
+                                CsvID = csv.GetField<int>("residencia_provincia_id"),
+                                Name = csv.GetField<string>("residencia_provincia_nombre")
+                            };
+
+                            _context.Provinces.Add(n);
+                            _context.SaveChanges();
+
+                            provinceMap.Add(n.CsvID, n);
+                        }
+
+                        if (!provinceMap.ContainsKey(csv.GetField<int>("carga_provincia_id"))) {
+                            var n = new Province {
+                                CsvID = csv.GetField<int>("carga_provincia_id"),
+                                Name = csv.GetField<string>("carga_provincia_nombre")
+                            };
+
+                            _context.Provinces.Add(n);
+                            _context.SaveChanges();
+
+                            provinceMap.Add(n.CsvID, n);
+                        }
+
+                        if (!districtMap.ContainsKey(csv.GetField<int>("residencia_departamento_id"))) {
+                            var n = new District {
+                                CsvID = csv.GetField<int>("residencia_departamento_id"),
+                                Name = csv.GetField<string>("residencia_departamento_nombre")
+                            };
+
+                            _context.Districts.Add(n);
+                            _context.SaveChanges();
+
+                            districtMap.Add(n.CsvID, n);
+                        }
+
+                        Case c = new Case {
+                            CsvID = csv.GetField<int>("id_evento_caso"),
+                            Sex = csv.GetField<string>("sexo"),
+                            Age = csv.GetField<int?>("edad"),
+                            ResidenceCountryName = csv.GetField<string>("residencia_pais_nombre"),
+                            ResidenceProvinceID = provinceMap[csv.GetField<int>("residencia_provincia_id")].ID,
+                            ResidenceDistrictID = districtMap[csv.GetField<int>("residencia_departamento_id")].ID,
+                            LoaderProvinceID = provinceMap[csv.GetField<int>("carga_provincia_id")].ID,
+                            SymptomsStartDate = csv.GetField<DateTime?>("fecha_inicio_sintomas"),
+                            DiagnoseDate = csv.GetField<DateTime?>("fecha_diagnostico"),
+                            CaseOpeningDate = csv.GetField<DateTime?>("fecha_apertura"),
+                            SepiOpening = csv.GetField<int?>("sepi_apertura"),
+                            AdmissionDate = csv.GetField<DateTime?>("fecha_internacion"),
+                            IntensiveCare = SpanishStringToBool(csv.GetField<string>("cuidado_intensivo")),
+                            IntensiveCareDate = csv.GetField<DateTime?>("fecha_cui_intensivo"),
+                            Deceased = SpanishStringToBool(csv.GetField<string>("fallecido")),
+                            DeceasedDate = csv.GetField<DateTime?>("fecha_fallecimiento"),
+                            Respirator = SpanishStringToBool(csv.GetField<string>("asistencia_respiratoria_mecanica")),
+                            PublicFounding = (csv.GetField<string>("origen_financiamiento") != "Privado"),
+                            Classification = csv.GetField<string>("clasificacion_resumen"),
+                            ClassificationDetail = csv.GetField<string>("clasificacion_resumen"),
+                            CSVLastUpdated = csv.GetField<DateTime>("ultima_actualizacion")
                         };
 
-                        _context.Provinces.Add(n);
-                        _context.SaveChanges();
+                        if (csv.GetField<int>("id_evento_caso") > maxCsvID) {
+                            cases.Add(c);
+                            currAdded++;
+                        }
+                        else {
+                            casesUpdate.Add(c);
+                            currUpdated++;
+                        }
 
-                        provinceMap.Add(n.CsvID, n);
+
+
+                        if (currAdded > 0 && currAdded % 100000 == 0) {
+                            uploader.Insert(cases);
+                            cases = new List<Case>();
+
+                            _context.SaveChanges();
+                        }
+
+                        if(currUpdated > 0 && currUpdated % 100000 == 0) {
+                            uploader.Update(casesUpdate);
+                            casesUpdate = new List<Case>();
+
+                            _context.SaveChanges();
+                        }
                     }
 
-                    if (!provinceMap.ContainsKey(csv.GetField<int>("carga_provincia_id"))) {
-                        var n = new Province {
-                            CsvID = csv.GetField<int>("carga_provincia_id"),
-                            Name = csv.GetField<string>("carga_provincia_nombre")
-                        };
+                    uploader.Insert(cases);
+                    uploader.Update(casesUpdate);
+                    _context.SaveChanges();
 
-                        _context.Provinces.Add(n);
-                        _context.SaveChanges();
+                    _context.Add(
+                        new DatabaseUpdate {
+                            Timestamp = DateTime.Now,
+                            LoadedRows = currAdded + currUpdated,
+                            InsertedRows = currAdded,
+                            UpdatedRows = currUpdated
+                        }
+                    );
+                    _context.SaveChanges();
 
-                        provinceMap.Add(n.CsvID, n);
-                    }
-
-                    if (!districtMap.ContainsKey(csv.GetField<int>("residencia_departamento_id"))) {
-                        var n = new District {
-                            CsvID = csv.GetField<int>("residencia_departamento_id"),
-                            Name = csv.GetField<string>("residencia_departamento_nombre")
-                        };
-
-                        _context.Districts.Add(n);
-                        _context.SaveChanges();
-
-                        districtMap.Add(n.CsvID, n);
-                    }
-
-                    Case c = new Case {
-                        CsvID = csv.GetField<int>("id_evento_caso"),
-                        Sex = csv.GetField<string>("sexo"),
-                        Age = csv.GetField<int?>("edad"),
-                        ResidenceCountryName = csv.GetField<string>("residencia_pais_nombre"),
-                        ResidenceProvinceID = provinceMap[csv.GetField<int>("residencia_provincia_id")].ID,
-                        ResidenceDistrictID = districtMap[csv.GetField<int>("residencia_departamento_id")].ID,
-                        LoaderProvinceID = provinceMap[csv.GetField<int>("carga_provincia_id")].ID,
-                        SymptomsStartDate = csv.GetField<DateTime?>("fecha_inicio_sintomas"),
-                        DiagnoseDate = csv.GetField<DateTime?>("fecha_diagnostico"),
-                        CaseOpeningDate = csv.GetField<DateTime?>("fecha_apertura"),
-                        SepiOpening = csv.GetField<int?>("sepi_apertura"),
-                        AdmissionDate = csv.GetField<DateTime?>("fecha_internacion"),
-                        IntensiveCare = SpanishStringToBool(csv.GetField<string>("cuidado_intensivo")),
-                        IntensiveCareDate = csv.GetField<DateTime?>("fecha_cui_intensivo"),
-                        Deceased = SpanishStringToBool(csv.GetField<string>("fallecido")),
-                        DeceasedDate = csv.GetField<DateTime?>("fecha_fallecimiento"),
-                        Respirator = SpanishStringToBool(csv.GetField<string>("asistencia_respiratoria_mecanica")),
-                        PublicFounding = (csv.GetField<string>("origen_financiamiento") != "Privado"),
-                        Classification = csv.GetField<string>("clasificacion_resumen"),
-                        ClassificationDetail = csv.GetField<string>("clasificacion_resumen"),
-                        CSVLastUpdated = csv.GetField<DateTime>("ultima_actualizacion")
-                    };
-
-                    if (csv.GetField<int>("id_evento_caso") > maxCsvID) {
-                        cases.Add(c);
-                        currAdded++;
-                    }
-                    else {
-                        casesUpdate.Add(c);
-                        currUpdated++;
-                    }
-
-
-
-                    if (currAdded > 0 && currAdded % 100000 == 0) {
-                        uploader.Insert(cases);
-                        cases = new List<Case>();
-
-                        _context.SaveChanges();
-                    }
-
-                    if(currUpdated > 0 && currUpdated % 100000 == 0) {
-                        uploader.Update(casesUpdate);
-                        casesUpdate = new List<Case>();
-
-                        _context.SaveChanges();
-                    }
+                    return Ok(new {
+                        Loaded = currAdded,
+                        Updated = currUpdated
+                    });
                 }
-
-                uploader.Insert(cases);
-                uploader.Update(casesUpdate);
-                _context.SaveChanges();
-
-                _context.Add(
-                    new DatabaseUpdate {
-                        Timestamp = DateTime.Now,
-                        LoadedRows = currAdded + currUpdated,
-                        InsertedRows = currAdded,
-                        UpdatedRows = currUpdated
-                    }
-                );
-                _context.SaveChanges();
-
-                return Ok(new {
-                    Loaded = currAdded,
-                    Updated = currUpdated
-                });
             }
         }
     }
